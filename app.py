4c88171236dff38f413980415cd2f71a2fb25ce2
Pushkar Narkhede <pushkarnarkhede01@gmail.com>
2023-04-26 21:30:24 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 import warnings
 warnings.filterwarnings("ignore")
-# import numpy as np
+import numpy as np
 import pickle
-import sklearn
+import random
 
 model = pickle.load(open('Selected_kmeans.pkl','rb'))
 
@@ -15,39 +15,42 @@ def home():
 
 @app.route('/pred', methods=['POST'])
 def predict():
-    acc_x = request.form.get('acc_x')
-    acc_y = request.form.get('acc_y')
-    acc_z = request.form.get('acc_z')
-    magnitude = request.form.get('magnitude')
-    motion = request.form.get('motion')
-    BPM = request.form.get('BPM')
+    acc_x = float(request.form.get('acc_x'))
+    acc_y = float(request.form.get('acc_y'))
+    acc_z = float(request.form.get('acc_z'))
 
-    if motion == 'walking':
-        motion = 4
-    elif motion == 'movement':
-        motion = 0
-    elif motion == 'running':
-        motion = 1
-    elif motion == 'steady':
-        motion = 2
+    magnitude = np.sqrt(((acc_x)** 2) + ((acc_y)** 2) + ((acc_z)** 2))
+
+    motion = 2 if 0.2 < magnitude < 0.5 else \
+             0 if 0.5 < magnitude < 1.0 else \
+             4 if 1.0 < magnitude < 1.7 else \
+             1 if magnitude > 1.7 else \
+             3
+
+    ranges = {
+        2: (60, 90),
+        4: (80, 136),
+        0: (70, 113),
+        1: (100, 157),
+        3: (60, 157)
+    }
+
+    if motion in ranges:
+        lower, upper = ranges[motion]
+        BPM = int(random.randint(lower, upper))
     else:
-        motion = 3
+        BPM = int(random.randint(60, 157))
 
-    acc_x = float(acc_x)
-    acc_y = float(acc_y)
-    acc_z = float(acc_z)
-    magnitude = float(magnitude)
-    BPM = int(BPM)
+    print(magnitude)
+    print(motion)
+    print(BPM)
 
     inputData = [[acc_x, acc_y, acc_z, magnitude, motion, BPM]]
     stress = model.predict(inputData)[0]
 
-    if (stress == 0):
-        raga = 'High Stress'
-    elif (stress == 1):
-        raga = 'Normal Stress'
-    else:
-        raga = 'Low Stress'
+    raga = 'High Stress' if stress == 0 else \
+           'Normal Stress' if stress == 1 else \
+           'Low Stress'
 
     return jsonify({'Stress level': str(stress), 'Stress type': raga})
 
